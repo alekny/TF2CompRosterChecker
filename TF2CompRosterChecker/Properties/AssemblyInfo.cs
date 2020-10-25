fcc853d70fe903d250e813a673c5ac2a583d74de
alekny <alekny@mailbox.org>
2020-10-25 06:47:04 +0100

    added timeout of 8 seconds to rgl queries, fixed crash on some invalid rgl queries

--- TF2CompRosterChecker/Properties/AssemblyInfo.cs
+++ TF2CompRosterChecker/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Windows;
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
 [assembly: AssemblyTitle("TF2CompRosterChecker")]
-[assembly: AssemblyDescription("Checks ETF2L/UGC-Roster via status output")]
+[assembly: AssemblyDescription("Checks ETF2L/RGL/UGC-Roster via status output")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TF2CompRosterChecker")]
@@ -51,7 +51,7 @@ using System.Windows;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.0.0")]
-[assembly: AssemblyFileVersion("1.5.0.0")]
+[assembly: AssemblyVersion("1.5.1.0")]
+[assembly: AssemblyFileVersion("1.5.1.0")]
 [assembly: NeutralResourcesLanguage("en")]
 
